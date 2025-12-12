4ccb800e1f8b09db9fbf1ac66c21d1a37371edd6
Kuldeepsinh Vaghela <109541617+Kuldeepsinh-Vaghela@users.noreply.github.com>
2025-12-12 13:22:17 -0500

    12-12-2025 0169-majority-element Easy

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         num_freq = {}
+        len_nums = len(nums)
         for num in nums:
             num_freq[num] = num_freq.get(num,0) + 1
-            if num_freq[num] > len(nums)/2:
+            if num_freq[num] > len_nums/2:
                 return num
         # for key,value in num_freq.items():
         #     if value > len(nums)/2:
