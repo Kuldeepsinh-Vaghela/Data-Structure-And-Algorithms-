51621c7a23f7fb9ddbd5ac23f30932cf22ff09cd
Kuldeepsinh Vaghela <109541617+Kuldeepsinh-Vaghela@users.noreply.github.com>
2025-12-13 12:54:04 -0500

    12-13-2025 0219-contains-duplicate-ii Easy

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,17 +1,27 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # map_dic = {}
+        # cntr = 0
+        # while cntr <= len(nums)-1:
+        #     if nums[cntr] not in map_dic:
+        #         map_dic[nums[cntr]] = cntr
+        #         cntr += 1
+        #     else:
+        #         if (cntr - map_dic[nums[cntr]])<= k:
+        #             return True
+        #         else:
+        #             map_dic[nums[cntr]] = cntr
+        #             cntr += 1 
+        # return False
         map_dic = {}
-        cntr = 0
-        while cntr <= len(nums)-1:
-            if nums[cntr] not in map_dic:
-                map_dic[nums[cntr]] = cntr
-                cntr += 1
+        for i, num in enumerate(nums):
+            if num not in map_dic:
+                map_dic[num] = i
             else:
-                if (cntr - map_dic[nums[cntr]])<= k:
+                if (i - map_dic[num])<= k:
                     return True
                 else:
-                    map_dic[nums[cntr]] = cntr
-                    cntr += 1 
+                    map_dic[num] = i
         return False
 
 
